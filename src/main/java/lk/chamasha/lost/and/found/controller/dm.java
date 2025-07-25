5cc4363b9cf7e1dc342774345b1ed7eab3930750
ChamashaFernando <chamashanethmi81@gmail.com>
2025-07-25 21:57:24 +0530

    update Notification impl class

--- src/main/java/lk/chamasha/lost/and/found/controller/dm.java
+++
@@ -1,4 +0,0 @@
-package lk.chamasha.lost.and.found.controller;
-
-public class dm {
-}
--- src/main/java/lk/chamasha/lost/and/found/service/NotificationService.java
+++ src/main/java/lk/chamasha/lost/and/found/service/NotificationService.java
@@ -1,10 +1,12 @@
 package lk.chamasha.lost.and.found.service;
 
 import lk.chamasha.lost.and.found.controller.response.NotificationResponse;
+import lk.chamasha.lost.and.found.exception.NotificationNotFoundException;
 
 import java.util.List;
 
 public interface NotificationService {
-    List<NotificationResponse> getUserNotifications(Long userId);
-    void markAsRead(Long id);
+    public List<NotificationResponse> getUserNotifications(Long userId);
+    public void markAsRead(Long id)throws NotificationNotFoundException;
+
 }
