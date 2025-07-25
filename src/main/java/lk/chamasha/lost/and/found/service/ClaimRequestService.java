f3a3bc19d4ba1b2f78a586c83185d7af979af6ab
ChamashaFernando <chamashanethmi81@gmail.com>
2025-07-25 22:40:30 +0530

    update ClaimRequestController

--- src/main/java/lk/chamasha/lost/and/found/service/ClaimRequestService.java
+++ src/main/java/lk/chamasha/lost/and/found/service/ClaimRequestService.java
@@ -2,11 +2,14 @@ package lk.chamasha.lost.and.found.service;
 
 import lk.chamasha.lost.and.found.controller.request.ClaimRequestRequest;
 import lk.chamasha.lost.and.found.controller.response.ClaimRequestResponse;
+import lk.chamasha.lost.and.found.exception.ClaimRequestNotFoundException;
+import lk.chamasha.lost.and.found.exception.ItemNotFoundException;
+import lk.chamasha.lost.and.found.exception.UserNotFoundException;
 
 import java.util.List;
 
 public interface ClaimRequestService {
-    ClaimRequestResponse createClaim(ClaimRequestRequest request);
-    List<ClaimRequestResponse> getClaimsByItem(Long itemId);
-    void approveClaim(Long claimId);
+    public ClaimRequestResponse createClaim(ClaimRequestRequest request)throws UserNotFoundException, ItemNotFoundException;
+    public List<ClaimRequestResponse> getClaimsByItem(Long itemId);
+    public void approveClaim(Long claimId)throws ClaimRequestNotFoundException;
 }
